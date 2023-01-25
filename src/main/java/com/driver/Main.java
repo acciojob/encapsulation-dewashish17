7d01f2002b26874f8a08d37aa64ea6e420bc796c
Dewashish Painkra <dewashishpainkra126@gmail.com>
2023-01-25 12:13:52 +0530

    final submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String [] args)
+    {
+        RWOnly obj=new RWOnly(); 
+        // Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+        // The field RWOnly.name is not visible
+
+        obj.setName("dewashish");
+        System.out.println(obj.getName());
+    }
 }
